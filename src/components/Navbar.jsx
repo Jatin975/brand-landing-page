9375ce25fb8a149a3ee0fe4c0efc1a2a3e531715
Jatin Paul <jatinpaul443@gmail.com>
2025-07-29 15:01:02 +0530

    Creating Responsive brand landing page design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,76 @@
+import { useState } from 'react';
 import nikeLogo from '../assets/nike_logo.png'
 import PrimaryButton from './PrimaryButton'
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
-export default function Navbar() {
+export default function Navbar({ isMobileView }) {
+    const [isOpen, setIsOpen] = useState(false);
+
     function handleNavlinkActive({ isActive }) {
         return `${isActive && "underline"} hover:underline`;
     }
 
     return (
         <>
-            <div className='flex justify-between py-5 sticky top-0 bg-white'>
-                <Link to="/">
-                    <img src={nikeLogo} alt="Logo" className='h-11 ml-20' ></img>
-                </Link>
-                <div>
-                    <ul className='flex gap-6 font-semibold text-gray-700'>
-                        <li>
-                            <NavLink to="/" className={handleNavlinkActive} end>Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="menu" className={handleNavlinkActive}>Menu</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="location" className={handleNavlinkActive}>Location</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="about" className={handleNavlinkActive} >About</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="contact" className={handleNavlinkActive}>Contact</NavLink>
-                        </li>
-                    </ul>
+            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <div className="flex items-center justify-between p-4">
+                    <Link to="/">
+                        <img src={nikeLogo} alt="Logo" className='h-11 md:ml-20' ></img>
+                    </Link>
+                    <div className='flex items-center'>
+
+                        <button data-collapse-toggle="navbar-default" type="button" onClick={() => setIsOpen((prev) => !prev)} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={isOpen}>
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                            </svg>
+                        </button>
+                        {!isMobileView &&
+                            <div className={` ${isOpen ? "block" : "hidden"} md:block flex justify-center`} id="navbar-default">
+                                <ul className="font-medium flex flex-col gap-1 p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                                    <li>
+                                        <NavLink to="/" className={handleNavlinkActive} end>Home</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to="menu" className={handleNavlinkActive}>Menu</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to="location" className={handleNavlinkActive}>Location</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to="about" className={handleNavlinkActive} >About</NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to="contact" className={handleNavlinkActive}>Contact</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        }
+                    </div>
+                    <PrimaryButton>Login</PrimaryButton>
                 </div>
-                <PrimaryButton>Login</PrimaryButton>
-            </div>
-            <Outlet />
+                {isMobileView &&
+                    <div className={` ${isOpen ? "block" : "hidden"} md:block flex justify-center relative`} id="navbar-default">
+                        <ul className="w-fit px-7 font-medium flex flex-col gap-1 p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 absolute">
+                            <li>
+                                <NavLink to="/" className={handleNavlinkActive} end>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="menu" className={handleNavlinkActive}>Menu</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="location" className={handleNavlinkActive}>Location</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="about" className={handleNavlinkActive} >About</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="contact" className={handleNavlinkActive}>Contact</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                }
+            </nav>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import Navbar from './components/Navbar.jsx'
 import About from './pages/About.jsx'
 import Menu from './pages/Menu.jsx'
 import Contact from './pages/Contact.jsx'
 import Location from './pages/Location.jsx'
+import RootLayout from './pages/RootLayout.jsx'
 
 const router = createBrowserRouter([
   {
-    Component: Navbar,
+    Component: RootLayout,
     children: [
       {
         index: true,
